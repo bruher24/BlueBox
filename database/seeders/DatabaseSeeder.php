2d80f5f312540c16bded2985c97b01206f42a66d
bruher24 <glushkovd2424@gmail.com>
2025-05-20 18:14:42 +0400

    updated seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,8 +19,8 @@ class DatabaseSeeder extends Seeder
             ['name' => 'Тяжелый'],
             ['name' => 'Хрупкий'],
         ];
-        foreach ($categories as $category) {
+        collect($categories)->each(function ($category) {
             Category::create($category);
-        }
+        });
     }
 }
